a668877939713c8ecd390ff133269c061d7b81dd
Finift <finift@list.ru>
2022-12-07 01:20:57 +0300

    "API 3 week, ex.4-finally"

--- API 3-4.py
+++ API 3-4.py
@@ -6,29 +6,28 @@
 # platform - мобильное приложение или веб
 # Если метод не может определить какой-то из параметров, он выставляет значение Unknown.
 # Наша задача написать параметризированный тест. Этот тест должен брать из дата-провайдера User Agent и ожидаемые значения, GET-делать запрос с этим User Agent и убеждаться, что результат работы нашего метода правильный - т.е. в ответе ожидаемое значение всех трех полей.
-# Список User Agent и ожидаемых значений можно найти по этой ссылке: https://gist.github.com/KotovVitaliy/138894aa5b6fa442163561b5db6e2e26
-# ============================================================
-# На самом деле метод не всегда работает правильно. Ответом к задаче должен быть список из тех User Agent, которые вернули неправильным хотя бы один параметр, с указанием того, какой именно параметр неправильный.
+
 
 import requests
+import pytest_check as check
 
 
 dictionary = {
     'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30': {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'},
-    'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1': {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'},
-    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)': {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'},
+    'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1': {'platform': 'Mobile', 'browser': 'No', 'device': 'iOS'},
+    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)': {'platform': 'Unknown', 'browser': 'Unknown', 'device': 'Unknown'},
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0': {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'},
-    'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1': {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'}
+    'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1': {'platform': 'Mobile', 'browser': 'No', 'device': 'Unknown'}
 }
 
 
 class TestAgent:
     def test_user_agents(self):
-        url = 'https://playground.learnqa.ru/ajax/api/user_agent_check'
         for key, value in dictionary.items():
-            response = requests.get(url, headers={'User-Agent': key})
-            print(response.text)
-            assert response.json()['platform'] == value['platform'], 'Platform wrong'
-            assert response.json()['browser'] == value['browser'], 'browser wrong'
-            assert response.json()['device'] == value['device'], 'device wrong'
+            response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check", headers={"User-Agent": key})
+            check.equal(response.json()['platform'], value['platform']), 'Platform wrong'
+            check.equal(response.json()['browser'], value['browser']), 'browser wrong'
+            check.equal(response.json()['device'], value['device']), 'device wrong'
+            print(response.text, response.json()['platform'], response.json()['browser'], response.json()['device'])
+
 
